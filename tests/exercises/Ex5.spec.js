a660a8d1a9915361d69ddf1cb363b9be4123127a
Ganesh K <ganeshkannan627@gmail.com>
2024-01-31 19:31:50 +0530

    updating code

---
+++ tests/exercises/Ex5.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+import { expect, test } from '@playwright/test'
+
+let URL = 'https://www.letskodeit.com/practice';
+
+
+test('Single Select and Multi Select Dropdown', async ({page})=>{
+
+    await page.goto(URL);
+
+   
+
+});
