c4937a1d93934ad757b4b67fd5ccb9bed2bd6968
Ganesh K <ganeshkannan627@gmail.com>
2024-02-06 17:36:29 +0530

    hgjghjh

--- tests/exercises/Ex6.spec.js
+++ tests/exercises/Ex6.spec.js
@@ -5,8 +5,26 @@ import { expect, test } from '@playwright/test'
 test('Mouse Actions - Hover', async ({ page }) => {
 
     await page.goto('https://demo.opencart.com/');
-    await page.locator('//a[normalize-space()="Laptops & Notebooks"]').hover();
-    await page.locator('')
+
+    await page.waitForSelector('.nav-item');
+    const items = await page.locator('.nav-item').all();
+
+    // console.log(items.length)
+    for(const item of items){
+        // console.log(await item.locator('.nav-link.dropdown-toggle').textContent())
+        
+        if(await item.locator('.nav-link.dropdown-toggle').textContent()=='Laptops & Notebooks') {
+            await item.hover();
+            // await item.locator('div.dropdown-menu a').waitFor()
+            const options = await item.locator('div.dropdown-menu a').all()
+            for(const option of options) {
+                console.log(await option.textContent())
+            }
+            break
+        }
+     }
+    // await page.locator('//a[normalize-space()="Laptops & Notebooks"]').hover();
+    // await page.locator('')
 
 
     
